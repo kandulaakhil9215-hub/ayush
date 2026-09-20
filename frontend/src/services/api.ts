f1108e0d90fb7e086fe5634c1eb7456ebabfef7b
akhil-kandula <kandulaakhil9215@gmail.com>
2026-09-20 23:08:51 +0530

    Connect frontend to production backend

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+baseURL: "https://ayush-3-yg21.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
